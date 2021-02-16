4743e2f1082aecab3a604acec3d4fc6108b3e10c
Demys Cota <demys.cota@snowmanlabs.com>
2021-02-16 16:28:25 -0300

    tests and C.I

--- widget/jest.config.js
+++ widget/jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript',
+  testMatch: ['**/*.spec.js'],
   transform: {
     '^.+\\.vue$': 'vue-jest',
+    '^.+\\.(ts|tsx)$': 'ts-jest',
   },
 }
--- widget/src/views/Widget/Standby.vue
+++ widget/src/views/Widget/Standby.vue
@@ -16,6 +16,7 @@ export default defineComponent({
 
 <template>
   <div
+    id="widget-open-button"
     @click="() => $emit('onOpen')"
     class="fixed z-50 right-0 bottom-0 mb-5 mr-5 bg-brand-main rounded-full py-3 px-5 flex items-center shadow-xl cursor-pointer select-none animate__animated animate__fadeInUp animate__faster"
   >
